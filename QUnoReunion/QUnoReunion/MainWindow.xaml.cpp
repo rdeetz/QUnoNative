dcc4ea61fa18060e159d3ec452121664f8cb0fa6
Roger Deetz <rdeetz@gmail.com>
2021-01-15 08:41:45 -0500

    Updated.

--- QUnoReunion/QUnoReunion/MainWindow.xaml.cpp
+++ QUnoReunion/QUnoReunion/MainWindow.xaml.cpp
@@ -6,29 +6,30 @@
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+using namespace Mooville::QUno::Reunion;
+using namespace Mooville::QUno::Reunion::implementation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace winrt::Mooville::QUno::Reunion::implementation
+MainWindow::MainWindow()
 {
-    MainWindow::MainWindow()
-    {
-        InitializeComponent();
-    }
-
-    int32_t MainWindow::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainWindow::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
-    {
-        myButton().Content(box_value(L"Clicked"));
-    }
+    InitializeComponent();
 }
+
+int32_t MainWindow::MyProperty()
+{
+    throw hresult_not_implemented();
+}
+
+void MainWindow::MyProperty(int32_t /* value */)
+{
+    throw hresult_not_implemented();
+}
+
+/*
+void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+{
+    myButton().Content(box_value(L"Clicked"));
+}
+*/
