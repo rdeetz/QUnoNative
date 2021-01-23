169ff111ab94e1bac5a0f19b879d4eb23c35238e
Roger Deetz <rdeetz@gmail.com>
2021-01-23 09:08:12 -0500

    Let's give this project some direction.

---
+++ QUnoComponent/Model/Direction.h
@@ -0,0 +1,13 @@
+// QUnoComponent - Direction.h
+// 2021 Roger Deetz
+
+#pragma once
+
+namespace winrt::Mooville::QUno::Model::implementation
+{
+	enum class Direction
+	{
+		Clockwise,
+		Counterclockwise,
+	};
+}
