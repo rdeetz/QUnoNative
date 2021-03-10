0deb93f0cd5538d343e8e4de60790811812ffe3a
Roger Deetz <rdeetz@gmail.com>
2021-03-10 15:08:43 -0500

    Reformatted.

--- QUnoBare/App.cpp
+++ QUnoBare/App.cpp
@@ -15,28 +15,26 @@ using namespace winrt::Mooville::QUno::Model;
 
 namespace Mooville::QUno::Bare
 {
-    struct App : implements<App, IFrameworkViewSource, IFrameworkView>
+    struct FrameworkView : implements<FrameworkView, IFrameworkView>
     {
-        CompositionTarget m_target{ nullptr };
-        VisualCollection m_visuals{ nullptr };
-        Visual m_selected{ nullptr };
-        float2 m_offset{};
+        CompositionTarget _target{ nullptr };
+        VisualCollection _visuals{ nullptr };
+        Visual _selected{ nullptr };
+        float2 _offset{};
 
-        IFrameworkView CreateView()
+        void Initialize(CoreApplicationView const&)
         {
-            return *this;
-        }
-
-        void Initialize(CoreApplicationView const &)
-        {
-        }
-
-        void Load(hstring const&)
-        {
+            return;
         }
 
         void Uninitialize()
         {
+            return;
+        }
+
+        void Load(hstring const&)
+        {
+            return;
         }
 
         void Run()
@@ -46,76 +44,84 @@ namespace Mooville::QUno::Bare
 
             CoreDispatcher dispatcher = window.Dispatcher();
             dispatcher.ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
+
+            return;
         }
 
-        void SetWindow(CoreWindow const & window)
+        void SetWindow(CoreWindow const& window)
         {
             Compositor compositor;
             ContainerVisual root = compositor.CreateContainerVisual();
-            m_target = compositor.CreateTargetForCurrentView();
-            m_target.Root(root);
-            m_visuals = root.Children();
+            _target = compositor.CreateTargetForCurrentView();
+            _target.Root(root);
+            _visuals = root.Children();
 
-            window.PointerPressed({ this, &App::OnPointerPressed });
-            window.PointerMoved({ this, &App::OnPointerMoved });
+            window.PointerPressed({ this, &FrameworkView::OnPointerPressed });
+            window.PointerMoved({ this, &FrameworkView::OnPointerMoved });
 
             window.PointerReleased([&](auto && ...)
             {
-                m_selected = nullptr;
+                _selected = nullptr;
             });
+
+            return;
         }
 
-        void OnPointerPressed(IInspectable const &, PointerEventArgs const & args)
+        void OnPointerPressed(IInspectable const&, PointerEventArgs const& args)
         {
-            Card card(winrt::Mooville::QUno::Model::Color::Red, Value::Five);
+            //Card card(winrt::Mooville::QUno::Model::Color::Red, Value::Five);
 
             float2 const point = args.CurrentPoint().Position();
 
-            for (Visual visual : m_visuals)
+            for (Visual visual : _visuals)
             {
                 float3 const offset = visual.Offset();
                 float2 const size = visual.Size();
 
-                if (point.x >= offset.x &&
-                    point.x < offset.x + size.x &&
-                    point.y >= offset.y &&
-                    point.y < offset.y + size.y)
+                if ((point.x >= offset.x) &&
+                    (point.x < offset.x + size.x) &&
+                    (point.y >= offset.y) &&
+                    (point.y < offset.y) + size.y)
                 {
-                    m_selected = visual;
-                    m_offset.x = offset.x - point.x;
-                    m_offset.y = offset.y - point.y;
+                    _selected = visual;
+                    _offset.x = offset.x - point.x;
+                    _offset.y = offset.y - point.y;
                 }
             }
 
-            if (m_selected)
+            if (_selected)
             {
-                m_visuals.Remove(m_selected);
-                m_visuals.InsertAtTop(m_selected);
+                _visuals.Remove(_selected);
+                _visuals.InsertAtTop(_selected);
             }
             else
             {
                 AddVisual(point);
             }
+
+            return;
         }
 
-        void OnPointerMoved(IInspectable const &, PointerEventArgs const & args)
+        void OnPointerMoved(IInspectable const&, PointerEventArgs const& args)
         {
-            if (m_selected)
+            if (_selected)
             {
                 float2 const point = args.CurrentPoint().Position();
 
-                m_selected.Offset(
+                _selected.Offset(
                 {
-                    point.x + m_offset.x,
-                    point.y + m_offset.y,
+                    point.x + _offset.x,
+                    point.y + _offset.y,
                     0.0f
                 });
             }
+
+            return;
         }
 
         void AddVisual(float2 const point)
         {
-            Compositor compositor = m_visuals.Compositor();
+            Compositor compositor = _visuals.Compositor();
             SpriteVisual visual = compositor.CreateSpriteVisual();
 
             static Windows::UI::Color colors[] =
@@ -145,18 +151,28 @@ namespace Mooville::QUno::Bare
                 0.0f,
             });
 
-            m_visuals.InsertAtTop(visual);
+            _visuals.InsertAtTop(visual);
 
-            m_selected = visual;
-            m_offset.x = -BlockSize / 2.0f;
-            m_offset.y = -BlockSize / 2.0f;
+            _selected = visual;
+            _offset.x = -BlockSize / 2.0f;
+            _offset.y = -BlockSize / 2.0f;
+
+            return;
+        }
+    };
+
+    struct FrameworkViewSource : implements<FrameworkViewSource, IFrameworkViewSource>
+    {
+        IFrameworkView CreateView()
+        {
+            return make<FrameworkView>();
         }
     };
 };
 
 int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 {
-    CoreApplication::Run(make<::Mooville::QUno::Bare::App>());
+    CoreApplication::Run(make<::Mooville::QUno::Bare::FrameworkViewSource>());
 
     return 0;
 }
