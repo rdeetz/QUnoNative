3c8eab831a2d4c91b7143d8d57ae8c76a9102e6b
Roger Deetz <rdeetz@gmail.com>
2021-12-19 11:00:50 -0500

    Updated title.

--- QUnoReunion/App.xaml.cpp
+++ QUnoReunion/App.xaml.cpp
@@ -32,6 +32,7 @@ App::App()
 void App::OnLaunched(LaunchActivatedEventArgs const&)
 {
     window = make<MainWindow>();
+    window.Title(L"QUno for the Windows App SDK");
     window.Activate();
 
     return;
